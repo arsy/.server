f262548ea220a1868ec7c3cbd5b0c5a61ca6d6ec
Yakup Arslan <yakup.arslan@monodijital.com>
2019-09-23 09:42:03 +0300

    initial db fix

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,1 +1,1 @@
-CREATE DATABASE `default_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE IF NOT EXISTS `default_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
